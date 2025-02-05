4d7734d148f34586266396128d80b36cbe3e7986
robineisepoptahti <155745755+robineisepoptahti@users.noreply.github.com>
2025-02-05 10:23:28 +0200

    Added working version of primitive backend

---
+++ app.js
@@ -0,0 +1,21 @@
+const notesRouter = require('./controllers/blogs')
+const express = require('express')
+const app = express()
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+const cors = require('cors')
+const mongoose = require('mongoose')
+
+MONGODB_URI = config.MONGODB_URI
+
+
+const mongoUrl = process.env.MONGODB_URI
+mongoose.connect(mongoUrl)
+
+app.use(cors())
+app.use(express.json())
+app.use('/api/blogs', notesRouter)
+
+
+
+module.exports = app
