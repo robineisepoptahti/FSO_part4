c2b92044df97858cfda564d9d1749ff66ff0e9eb
robineisepoptahti <155745755+robineisepoptahti@users.noreply.github.com>
2025-02-07 15:40:54 +0200

    added has most blogs

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,6 +1,9 @@
+const _ = require('lodash')
+
 const dummy = (blogs) => {
     return 1
   }
+
 
 const totalLikes = (blogs) => {
   const reducer = (sum, item) => {
@@ -9,8 +12,40 @@ const totalLikes = (blogs) => {
   return blogs.reduce(reducer, 0)
 }
   
+
+
+const favoriteBlog = (blogs) => {
+if (!blogs || blogs.length === 0) {
+  return 0
+} 
+const reducer = (fav, item) => {
+    if (fav.likes < item.likes) {
+      fav = item 
+    }
+    return fav
+  }
+  return blogs.reduce(reducer, blogs[0])
+}
+
+const mostBlogs = (blogs) => {
+  if (!blogs || blogs.length === 0) {
+    return 0;
+  }
+  const reducer = (most, item) => {
+    const blogscpy = _.filter(blogs, (n) => item.author === n.author);
+    if (blogscpy.length > most.blogs) {
+      return { author: item.author, blogs: blogscpy.length };
+    }
+    return most;
+  }
+  return blogs.reduce(reducer, { author: "None", blogs: 0 });
+}
+
+
   module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog,
+    mostBlogs
   }
 
