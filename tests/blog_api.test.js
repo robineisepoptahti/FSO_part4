52e9488fe53a4791f95372508d005bdd71cce779
robineisepoptahti <155745755+robineisepoptahti@users.noreply.github.com>
2025-02-08 20:58:25 +0200

    up until 4.11

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -23,6 +23,19 @@ const initialBlogs = [
     }
   ]
 
+const blogToPost = {
+    title: "Book3",
+    author: "Darwin3",
+    url: "url3",
+    likes: 3
+} 
+
+const likelessBlog = {
+  title: "Book4",
+  author: "Darwin4",
+  url: "url4",
+} 
+
   describe('when there is initially 2 blogs saved', () => {
 beforeEach(async () => {
     await Blog.deleteMany({})
@@ -49,4 +62,43 @@ test('check if id is defined in response', async () => {
     res.body.forEach(blog => assert(blog.id))
 })
 
+test('post 1 blog', async () =>
+{
+    //Here we are getting the length before adding
+    res_len = await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+
+//adding 1 blog.
+    const res_new_len = await api
+        .post('/api/blogs')
+        .send(blogToPost)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
+assert.strictEqual(res_new_len.body.title, blogToPost.title)
+assert.strictEqual(res_new_len.body.url, blogToPost.url)
+assert.strictEqual(res_new_len.body.likes, blogToPost.likes)
+assert.strictEqual(res_new_len.body.author, blogToPost.author)
+
+//Getting the updated list of blogs
+const post_res_len = await api
+.get('/api/blogs')
+.expect(200)
+.expect('Content-Type', /application\/json/)
+assert.strictEqual(res_len.body.length + 1, post_res_len.body.length)
+}
+)
+
+test('test if likes is 0 when not defined', async () => {
+  //Cleaning the database
+  await Blog.deleteMany({}) 
+  //Adding a blog without likes
+  const res = await api
+  .post('/api/blogs')
+  .send(likelessBlog)
+  .expect(201)
+  .expect('Content-Type', /application\/json/)
+  assert.strictEqual(res.body.likes, 0)
+})
 })
